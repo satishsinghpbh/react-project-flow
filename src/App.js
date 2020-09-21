18c6d50d7beb68d750181e56b8640e64960831b2
Satish Singh <pbh.satish@gmail.com>
2020-09-21 10:26:10 -0400

    Flow small POC

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@
 import * as React from 'react';
 
 const ARTICLES = [
-  { id: '0', title: 'How to fetch data in React', url: 'https://www.robinwieruch.de/react-fetching-data/' },
-  { id: '1', title: 'React\'s Ecosystem as a flexible Framework', url: 'https://www.robinwieruch.de/essential-react-libraries-framework/' },
-  { id: '2', title: 'What\'s new in React 16?', url: 'https://www.robinwieruch.de/what-is-new-in-react-16/' },
-  { id: '3', title: '8 things to learn in React before using Redux', url: 'https://www.robinwieruch.de/learn-react-before-using-redux/' },
-  { id: '4', title: 'Accept Stripe Payments with React and Express', url: 'https://www.robinwieruch.de/react-express-stripe-payment/' },
-  { id: '5', title: 'Tips to learn React + Redux', url: 'https://www.robinwieruch.de/tips-to-learn-react-redux/' },
-  { id: '6', title: '10 Reasons why I moved from Angular to React', url: 'https://www.robinwieruch.de/reasons-why-i-moved-from-angular-to-react/' },
-  { id: '7', title: 'All the Conditional Renderings in React', url: 'https://www.robinwieruch.de/conditional-rendering-react/' },
-  { id: '8', title: 'Redux or MobX: An attempt to dissolve the Confusion', url: 'https://www.robinwieruch.de/redux-mobx-confusion/' },
-  { id: '9', title: 'Tips to learn React + Redux', url: 'https://www.robinwieruch.de/tips-to-learn-react-redux/' },
-  { id: '10', title: 'A gentle Introduction to React\'s Higher Order Components', url: 'https://www.robinwieruch.de/gentle-introduction-higher-order-components/' },
-  { id: '11', title: 'Complete Course to learn Redux and MobX', url: 'https://roadtoreact.com/' },
+  { id: '0', title: 'How to fetch data in React0', url: '/abc/xyz' },
+  { id: '1', title: 'How to fetch data in React1', url: '/abc/xyz' },
+  { id: '2', title: 'How to fetch data in React2', url: '/abc/xyz' },
+  { id: '3', title: 'How to fetch data in React3', url: '/abc/xyz' },
+  { id: '4', title: 'How to fetch data in React4', url: '/abc/xyz' },
+  { id: '5', title: 'How to fetch data in React5', url: '/abc/xyz' },
+  { id: '6', title: 'How to fetch data in React6', url: '/abc/xyz' },
+  { id: '7', title: 'How to fetch data in React7', url: '/abc/xyz' },
+  { id: '8', title: 'How to fetch data in React8', url: '/abc/xyz' },
+  { id: '9', title: 'How to fetch data in React9', url: '/abc/xyz' },
+  { id: '10', title: 'How to fetch data in React10', url: '/abc/xyz' },
+  { id: '11', title: 'How to fetch data in React11', url: '/abc/xyz' },
 ];
 
 type AppProps = {
@@ -62,7 +62,7 @@ class App extends React.Component<AppProps, AppState> {
 
         <Articles articles={ARTICLES.filter(applyFilter(searchTerm))} />
 
-        <p>Found in <a href="https://roadtoreact.com/">the Road to learn React</a></p>
+        <p>Found in <a href="https://reactjs.org/docs/getting-started.html">the Road to learn React</a></p>
       </div>
     );
   }
